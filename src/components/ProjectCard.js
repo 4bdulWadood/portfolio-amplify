fed99c7b7300b513802b3bd073f31ef14778548a
4bdulWadood <57127141+4bdulWadood@users.noreply.github.com>
2023-08-20 16:04:48 -0400

    Finished projects card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,8 +1,6 @@
 import "../public/styles/projectscard.css";
-import { useEffect, useRef } from "react";
 import { Button } from '../components/StyledButton';
-import { SkillWrapper } from './Skill'
-import ReactLogo from "../assets/ReactLogo.png";
+
 
 function ProjectCard() {
   return (
@@ -11,7 +9,24 @@ function ProjectCard() {
         <div className="text">
             <div className="header">
                 <div>InvoiceAI</div>
-               
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/768px-React-icon.svg.png?20220125121207"
+                    alt="Example Image"
+                    width="15"
+                    height="13"
+                />
+               <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/1200px-Scikit_learn_logo_small.svg.png"
+                    alt="Example Image"
+                    width="16"
+                    height="12"
+                />
+               <img
+                    src="https://pbs.twimg.com/profile_images/1417542931209199621/fWMEIB5j_400x400.jpg"
+                    alt="Example Image"
+                    width="12"
+                    height="12"
+                />
             </div>
             <div className="body">
                 InvoiceAI leverages large language model technology to allow users to gain insights on their personal finances.
