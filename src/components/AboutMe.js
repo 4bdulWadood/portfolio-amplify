5da1b6fb76ed3ad2bc5fcb7aa28dfc8c00b29ee5
4bdulWadood <57127141+4bdulWadood@users.noreply.github.com>
2023-11-13 14:23:38 -0500

    Update AboutMe.js

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -38,7 +38,7 @@ export default function AboutMe() {
           <div className="text">
             When it comes to the work I do I dislike labels. I don't like to define myself as a backend, frontend, full-stack developer or cloud architect, but rather someone who seeks to understand and build quality software products. The 
             need to specialize in one field and pick a lane is important for staying employed in a competitve job market, however, having a holistic view of the software is equally important to build great software products as it enables transparent 
-            communication within an organization and it fosters product ownership in the employees. It's like being a versatile musician who can play multiple instruments - you might be talented at one instrument, but the ability to harmonize with others makes
+            communication within an organization, fosters product ownership in the employees, and leads to a faster go-to-market time for products. It's like being a versatile musician who can play multiple instruments - you might be talented at one instrument, but the ability to harmonize with others makes
             the ensemble shine.
             <br />
             <div className="MobileSpaceman"><Lottie style={style} interactivity={interactivity} animationData={Animated}/></div>
